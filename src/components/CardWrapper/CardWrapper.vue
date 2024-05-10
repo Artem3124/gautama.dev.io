8eddaf7fda0228b115ad8ccbcf1bd9d82d91166a
Artem3124 <mr.zhukovsky02@gmail.com>
2024-05-10 14:21:16 +0200

    wrapper create
    Card wrapper created, not sure about imageRandomizer though

---
+++ src/components/CardWrapper/CardWrapper.vue
@@ -0,0 +1,45 @@
+
+<template>
+    <!-- Provide global themes for block-themes -->
+    <div class="card-wrapper" :class="`block-theme-${theme}`">
+        <h1>Current theme is {{ theme }}</h1>
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+    import { getRandomImagePathByTheme } from './ImageRandomizer';
+
+export default {
+    props: {
+        theme: {
+            type: String,
+            required: true,
+        },
+        backgroundImage: { 
+            type: String, 
+            required: false,
+        },
+    },
+
+    data() {
+        return {
+            imagePath: getRandomImagePathByTheme(this.theme),
+        }
+    },
+}
+</script>
+
+<style>
+.card-wrapper {
+    background: url(this.imagePath);
+    background-color: red;
+    border-radius: 10px;
+    padding: 20px;
+    margin: 10px;
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+}
+</style>
---
+++ src/views/practices/PageEndPractice.vue
@@ -0,0 +1,39 @@
+<template>
+    <div>
+        <CardWrapper :theme="theme">
+        <p>Card wrapper has wrapped me!</p>
+        </CardWrapper>
+    </div>
+</template>
+
+<script>
+import CardWrapper from '@/components/CardWrapper/CardWrapper.vue';
+
+export default {
+    props: {
+        theme: {
+            type: String,
+            required: true,
+        },
+    },
+
+    components: {
+        CardWrapper
+    },
+
+    data() {
+        return {
+            id: this.theme,
+        };
+    },
+};
+
+</script>
+
+<style>
+.base-theme {}
+
+.piece-theme {}
+
+.concentration-theme {}
+</style>
