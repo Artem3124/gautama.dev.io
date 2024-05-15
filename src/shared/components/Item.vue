f9c923db21e3c46fa4d1d42f833d07aadc78f396
Artem3124 <mr.zhukovsky02@gmail.com>
2024-05-15 22:16:23 +0200

    item list initiate

---
+++ src/shared/components/Item.vue
@@ -0,0 +1,33 @@
+<template>
+    // background, underLine, itemDistance are the props that can be passed to the component
+    // insert logic varies here
+    <div class="">
+        <slot></slot>
+    </div>
+</template>
+
+<script>
+export default {
+    props: { 
+        background: {
+            type: Boolean,
+            required: false,
+            default: false,
+        },
+        underLine: {
+            type: Boolean,
+            required: false,
+            default: false,
+        },
+        itemDistance: {
+            type: String,
+            required: false,
+            default: "0em",
+        },
+    },
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/shared/components/ItemsList.vue
@@ -0,0 +1,40 @@
+<template>
+    <div>
+
+    </div>
+</template>
+
+<script>
+export default {
+    props: { 
+        // items: The items that will be displayed
+        items: {
+            type: Array,
+            required: true,
+            default: [],
+        },
+        // itemHeight: The height of the items
+        itemHeight: {
+            type: String,
+            required: false,
+            default: "55px",
+        },
+        // itemWidth: The width of the items
+        itemWidth: {
+            type: String,
+            required: false,
+            default: "55px",
+        },
+        // itemDistance: The distance between the items
+        itemDistance: {
+            type: String,
+            required: false,
+            default: "0em",
+        },
+    },
+}
+</script>
+
+<style>
+
+</style>
